dea74b3368f86ca5a44b0f513fad9b6a4b8423c4
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-06-09 03:00:00 +0000

    criação do repository - categoriaUsuarioRepository

---
+++ src/repository/categoriaUsuarioRepository.ts
@@ -0,0 +1,8 @@
+import { CategoriaUsuarioEntity } from "../models/categoriaUsuarioEntity";
+
+export const categoriasUsuario: CategoriaUsuarioEntity[] =
+[
+      new CategoriaUsuarioEntity(1, "Professor"),
+      new CategoriaUsuarioEntity(2, "Aluno"),
+      new CategoriaUsuarioEntity(3, "Bibliotecário"),
+]
