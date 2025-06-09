de95b643a6adfb0ce060f82952739793e5c3bcc1
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-06-09 03:00:38 +0000

    criação do repository - categoriaLivroRepository

---
+++ src/repository/categoriaLivroRepository.ts
@@ -0,0 +1,9 @@
+import { CategoriaLivroEntity } from "../models/categoriaLivroEntity";
+
+export const categoriasLivro : CategoriaLivroEntity[] =
+[
+    new CategoriaLivroEntity(1, "Romance"),
+    new CategoriaLivroEntity(2, "Computação"),
+    new CategoriaLivroEntity(3, "Letras"),
+    new CategoriaLivroEntity(4, "Gestão")
+]   
