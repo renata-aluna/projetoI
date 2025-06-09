2aa1aa94b32bf3df03c64eb419f158b8ab5b54a6
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-06-09 01:10:54 +0000

    criação dos endpoints - /estoque

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { UsuarioController } from "./controller/usuarioController"
 import { LivroController } from "./controller/livroController"
+import { ExemplarController } from "./controller/exemplarController"
 
 const usuarioController = new UsuarioController()
 const livroController = new LivroController()
+const exemplarController = new ExemplarController()
 
 const app = express()
 
@@ -21,5 +23,11 @@ app.get("/library/livros", livroController.buscaLivro.bind(livroController))
 app.get("/library/livros/:isbn", livroController.buscaLivroPorIsbn.bind(livroController))
 app.put("/library/livros/:isbn", livroController.atualizaLivro.bind(livroController))
 app.delete("/library/livros/:isbn", livroController.removeLivro.bind(livroController))
+
+app.post("/library/estoque", exemplarController.criaExemplar.bind(exemplarController))
+app.get("/library/estoque", exemplarController.listaExemplares.bind(exemplarController))
+app.get("/library/estoque/:codigo", exemplarController.buscaExemplarporCodigo.bind(exemplarController))
+app.put("/library/estoque/:codigo", exemplarController.atualizaExemplar.bind(exemplarController))
+app.delete("/library/estoque/:codigo", exemplarController.removeExemplar.bind(exemplarController))
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}. http://localhost:3090`))
