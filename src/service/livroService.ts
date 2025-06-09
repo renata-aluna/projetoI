4cc695e31f48dda20570f0c9bb3e90b1197636fb
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-06-09 09:18:27 +0000

    Criação das validações no service - livroService

--- src/service/livroService.ts
+++ src/service/livroService.ts
@@ -1,4 +1,5 @@
 import { LivroEntity } from "../models/livroEntity";
+import { categoriasLivro } from "../repository/categoriaLivroRepository";
 import { LivroRepository } from "../repository/livroRepository";
 
 export class LivroService{
@@ -8,6 +9,18 @@ export class LivroService{
         if (!data.titulo || !data.autor || !data.editora || !data.edicao || !data.isbn || !data.categoriaId){
             throw new Error("Campos obrigatórios não preenchidos")    
         }
+
+        if (!categoriasLivro.find(categoria => categoria.id === data.categoriaId)){
+            throw new Error("Categoria não existe!!!");
+        }
+
+        const livros = this.livroRepository.listaLivros(); 
+        const combinado = livros.find(livro => livro.autor == data.autor && livro.editora === data.editora && livro.edicao == data.edicao);
+
+        if (combinado) {
+            throw new Error("Já existe livro com essa combinação de autor, editora e edição.");
+        }
+
         const livro = new LivroEntity (undefined, data.titulo, data.autor, data.editora, data.edicao, data.isbn, data.categoriaId)
         this.livroRepository.cadastraLivro(livro)
 
@@ -28,9 +41,15 @@ export class LivroService{
         if (!data.titulo || !data.autor || !data.editora || !data.edicao || !data.isbn || !data.categoriaId){
             throw new Error("Campos obrigatórios não preenchidos")    
         }
+
         if(!livroExistente){
             throw new Error ("Livro não encontrado.")
         }
+
+        if (!categoriasLivro.find(categoria => categoria.id === data.categoriaId)){
+            throw new Error("Categoria não existe!!!");
+        }
+        
         const livroNovo = new LivroEntity(livroExistente.id, data.titulo, data.autor, data.editora, data.edicao, data.isbn, data.categoriaId)
         return this.livroRepository.atualizaLivro(isbn, livroNovo)
     }
