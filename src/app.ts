40a3043c4f65282e7b20274eaa10f2aed7d4ae59
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-06-09 07:09:58 +0000

    criação dos endpoints - /emprestimos

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import { ExemplarController } from "./controller/exemplarController"
 import { CategoriaUsuarioController } from "./controller/categoriaUsuarioController"
 import { CategoriaLivroController } from "./controller/categoriaLivroController"
 import { CursoController } from "./controller/cursoController"
+import { EmprestimoController } from "./controller/emprestimoController"
 
 const usuarioController = new UsuarioController()
 const livroController = new LivroController()
 const exemplarController = new ExemplarController()
+const emprestimoController = new EmprestimoController()
 const categoriaUsuarioController = new CategoriaUsuarioController()
 const categoriaLivroController = new CategoriaLivroController()
 const cursoController =new CursoController()
@@ -36,6 +38,10 @@ app.get("/library/estoque", exemplarController.listaExemplares.bind(exemplarCont
 app.get("/library/estoque/:codigo", exemplarController.buscaExemplarporCodigo.bind(exemplarController))
 app.put("/library/estoque/:codigo", exemplarController.atualizaExemplar.bind(exemplarController))
 app.delete("/library/estoque/:codigo", exemplarController.removeExemplar.bind(exemplarController))
+
+app.post("/library/emprestimos", emprestimoController.cadastrarEmprestimo.bind(emprestimoController))
+app.get("/library/emprestimos", emprestimoController.listaEmprestimo.bind(emprestimoController))
+app.put("/library/emprestimos/:id/devolucao", emprestimoController.DevolveEmprestimo.bind(emprestimoController))
 
 app.get("/library/catalogos/categorias-usuario", categoriaUsuarioController.listaCategoriasUsuario.bind(categoriaUsuarioController))
 app.get("/library/catalogos/categorias-livro", categoriaLivroController.listaCategoriasLivro.bind(categoriaLivroController))
