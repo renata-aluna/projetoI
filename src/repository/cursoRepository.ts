8464ca4a079380ddebc52871b26bcd6b4c2e67f6
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-06-09 03:01:08 +0000

    criação do repository - cursoRepository

---
+++ src/repository/cursoRepository.ts
@@ -0,0 +1,8 @@
+import { CursoEntity } from "../models/cursoEntity";
+
+export const cursos: CursoEntity[] = 
+[
+    new CursoEntity(1, "Administração"),
+    new CursoEntity(2, "ADS"),
+    new CursoEntity(3, "Pedagogia"),
+]
