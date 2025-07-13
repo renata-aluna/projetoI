d17dfccdc7e4d1a1d5f2fbe559e36ccef316f47c
renata-aluna <renata.l@aluno.ifsp.edu.br>
2025-07-13 16:20:00 -0300

    Alterações para uso do json do express no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,27 @@
 import express from "express"
-import { UsuarioController } from "./controller/usuarioController"
-import { LivroController } from "./controller/livroController"
-import { ExemplarController } from "./controller/exemplarController"
-import { CategoriaUsuarioController } from "./controller/categoriaUsuarioController"
-import { CategoriaLivroController } from "./controller/categoriaLivroController"
-import { CursoController } from "./controller/cursoController"
-import { EmprestimoController } from "./controller/emprestimoController"
+import { RegisterRoutes } from "./route/routes"
+import { setupSwagger} from "./config/swagger"
 import { inicializarTabelas } from "./database/inicializador"
 
-const usuarioController = new UsuarioController()
-const livroController = new LivroController()
-const exemplarController = new ExemplarController()
-const emprestimoController = new EmprestimoController()
-const categoriaUsuarioController = new CategoriaUsuarioController()
-const categoriaLivroController = new CategoriaLivroController()
-const cursoController =new CursoController()
+async function main() {
+  await inicializarTabelas()
 
+  const app = express()
+  app.use(express.json())
 
-const app = express()
+  const PORT = 3090
+  
+  const apiRouter = express.Router()
+  RegisterRoutes(apiRouter) 
+  app.use("/library", apiRouter)
 
-const PORT = 3090
-app.use(express.json())
+  setupSwagger(app) 
 
-app.post("/library/usuarios", usuarioController.criaUsuario.bind(usuarioController))
-app.get("/library/usuarios", usuarioController.buscaUsuario.bind(usuarioController))
-app.get("/library/usuarios/:cpf", usuarioController.buscaUsuarioPorCpf.bind(usuarioController))
-app.put("/library/usuarios/:cpf", usuarioController.atualizaUsuario.bind(usuarioController))
-app.delete("/library/usuarios/:cpf", usuarioController.removeUsuario.bind(usuarioController))
-
-app.post("/library/livros", livroController.criaLivro.bind(livroController))
-app.get("/library/livros", livroController.buscaLivro.bind(livroController))
-app.get("/library/livros/:isbn", livroController.buscaLivroPorIsbn.bind(livroController))
-app.put("/library/livros/:isbn", livroController.atualizaLivro.bind(livroController))
-app.delete("/library/livros/:isbn", livroController.removeLivro.bind(livroController))
-
-app.post("/library/estoque", exemplarController.criaExemplar.bind(exemplarController))
-app.get("/library/estoque", exemplarController.listaExemplares.bind(exemplarController))
-app.get("/library/estoque/:codigo", exemplarController.buscaExemplarporCodigo.bind(exemplarController))
-app.put("/library/estoque/:codigo", exemplarController.atualizaExemplar.bind(exemplarController))
-app.delete("/library/estoque/:codigo", exemplarController.removeExemplar.bind(exemplarController))
-
-app.post("/library/emprestimos", emprestimoController.cadastrarEmprestimo.bind(emprestimoController))
-app.get("/library/emprestimos", emprestimoController.listaEmprestimo.bind(emprestimoController))
-app.put("/library/emprestimos/:id/devolucao", emprestimoController.DevolveEmprestimo.bind(emprestimoController))
-app.put("/library/emprestimos/verificar-suspensoes", emprestimoController.verificarSuspensoesAutomaticas.bind(emprestimoController));
-
-app.get("/library/catalogos/categorias-usuario", categoriaUsuarioController.listaCategoriasUsuario.bind(categoriaUsuarioController))
-app.get("/library/catalogos/categorias-livro", categoriaLivroController.listaCategoriasLivro.bind(categoriaLivroController))
-app.get("/library/catalogos/cursos", cursoController.listarCursos.bind(cursoController))
-
-
-async function inicarAplicacoes() {
-        await inicializarTabelas()
-        app.listen(PORT, () => { console.log(`Servidor rodando em http://localhost:${PORT}/library`)})
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/library`)
+  })
 }
 
-inicarAplicacoes().catch((err) => {
+main().catch((err) => {
   console.error("Erro ao iniciar o servidor:", err)
-})
-
-import './database/mysql';
+});
